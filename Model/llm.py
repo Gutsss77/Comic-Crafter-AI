7ae17556878a8cf01ec68e65dfd543f83156f81d
Amrit  pal Singh <Newbieprogram22>
2025-03-16 15:10:40 +0530

    modify prompts, llm, app files

--- Model/llm.py
+++ Model/llm.py
@@ -1,5 +1,6 @@
 from langchain_community.llms import Ollama
 
-text_model="gemma3:latest"
+# text_model="gemma3:latest"
+text_model="hf.co/bartowski/ibm-granite_granite-3.2-2b-instruct-GGUF:Q4_K_S"
 
 text_llm = Ollama(model=text_model, temperature=0.5)
